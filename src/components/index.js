bedf0d693a9e0b5eb6e3240d3d50f6e8e9d7c638
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-19 21:16:51 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from './Navbar';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as HomePage } from './Homepage';
+export { default as News } from './News';
+export { default as Loader } from './Loader';
+export { default as LineChart } from './LineChart';
